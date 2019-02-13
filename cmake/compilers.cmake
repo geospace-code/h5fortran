0dfe8d38d16833d6487f2504f9b0ceca73c3f55d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-02-12 23:16:45 -0500

    simplify cmake
    minimum meson

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -12,21 +12,21 @@ if(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
   list(APPEND FFLAGS -warn -traceback)
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
   list(APPEND FFLAGS -march=native -fimplicit-none)
-  
+
   if(CMAKE_BUILD_TYPE STREQUAL Debug)
     list(APPEND FFLAGS -fcheck=all)
   endif()
-  
+
   if(CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 8)
     list(APPEND FFLAGS -std=f2018)
   else()
     list(APPEND FFLAGS -std=f2008)
   endif()
-  
+
   list(APPEND FFLAGS -Wall -Wextra -Wpedantic -Werror=array-bounds -Warray-temporaries)
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL PGI)
 
-elseif(CMAKE_Fortran_COMPILER_ID STREQUAL Flang) 
+elseif(CMAKE_Fortran_COMPILER_ID STREQUAL Flang)
   list(APPEND FFLAGS -Mallocatable=03)
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL NAG)
   list(APPEND FFLAGS -u -C=all)
@@ -34,14 +34,10 @@ endif()
 
 
 include(CheckFortranSourceCompiles)
-check_fortran_source_compiles("
-program a
-character :: b
-error stop b
-end program" 
+check_fortran_source_compiles("character :: b; error stop b; end"
   f18errorstop
   SRC_EXT f90)
-                              
+
 if(NOT f18errorstop)
   message(FATAL_ERROR "f2018 error stop with variable required, which is not supported by ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION}")
 endif()
--- cmake/hdf5.cmake
+++ cmake/hdf5.cmake
@@ -2,12 +2,7 @@ find_package(HDF5 REQUIRED COMPONENTS Fortran Fortran_HL)
 
 set(CMAKE_REQUIRED_INCLUDES ${HDF5_INCLUDE_DIRS} ${HDF5_Fortran_INCLUDE_DIRS})
 set(CMAKE_REQUIRED_LIBRARIES ${HDF5_Fortran_LIBRARIES} ${HDF5_Fortran_HL_LIBRARIES})
-check_fortran_source_compiles("
-program a
-use h5lt
-end" 
-  hasHDF5
-  SRC_EXT f90)
+check_fortran_source_compiles("use h5lt; end" hasHDF5 SRC_EXT f90)
 
 if(NOT hasHDF5)
   message(FATAL_ERROR "HDF5 library not working with ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION}")
