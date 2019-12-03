fea917bb4047893a6fb11f6653656a1d542e8664
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-12-02 23:50:10 -0500

    specify build dir, use python for post-proc

--- tests/GetShape.f90
+++ tests/GetShape.f90
@@ -1,6 +1,6 @@
 !! This program shows how HDF5 dimension orders are distinct in different langauges
 use hdf5_interface, only: hdf5_file,hsize_t
-use, intrinsic:: iso_fortran_env, only: real64, stdout=>output_unit
+use, intrinsic:: iso_fortran_env, only: real64, stdout=>output_unit, stderr=>error_unit
 implicit none
 
 type(hdf5_file) :: h5f
@@ -8,6 +8,7 @@ character(1024) :: argv
 character(:), allocatable :: fn, dname, cmd
 integer(HSIZE_T), allocatable :: dims(:)
 integer :: ierr
+logical :: exists
 
 if (command_argument_count() /= 2) error stop "filename dset_name"
 
@@ -16,6 +17,12 @@ fn = trim(argv)
 
 call get_command_argument(2, argv)
 dname = trim(argv)
+
+inquire(file=fn, exist=exists)
+if (.not. exists) then
+  write(stderr, '(A,A)'), fn, ' is not a file.'
+  error stop 77
+endif
 
 call h5f%initialize(fn, status='old', action='r')
 
