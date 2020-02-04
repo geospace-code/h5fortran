690f0d22234eceddb53536435312f17e60e64d08
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-04 12:01:24 -0500

    build cleanup

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,9 +1,3 @@
-if(NOT PROJECT_SOURCE_DIR STREQUAL CMAKE_SOURCE_DIR)
-  # this will NOT return for ExternalProject
-  # this will return for FetchContent
-  return()
-endif()
-
 if(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
   if(WIN32)
     set(CMAKE_Fortran_FLAGS "/stand:f18 /traceback /warn /heap-arrays ")
@@ -11,17 +5,13 @@ if(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
     set(CMAKE_Fortran_FLAGS "-stand f18 -traceback -warn -heap-arrays ")
   endif()
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  string(APPEND CMAKE_Fortran_FLAGS "-march=native -fimplicit-none ")
-
-  if(CMAKE_BUILD_TYPE STREQUAL Debug)
-    string(APPEND CMAKE_Fortran_FLAGS "-fcheck=all ")
-  endif()
+  string(APPEND CMAKE_Fortran_FLAGS "-fimplicit-none -Wall -Wextra ")
+  string(APPEND CMAKE_Fortran_FLAGS_DEBUG "-fcheck=all ")
+  add_compile_options(-march=native)
 
   if(CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 8)
     string(APPEND CMAKE_Fortran_FLAGS "-std=f2018 ")
   endif()
-
-  string(APPEND CMAKE_Fortran_FLAGS "-Wall -Wextra -Wpedantic ")
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL PGI)
   set(CMAKE_Fortran_FLAGS "-C -Mdclchk ")
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL Flang)
