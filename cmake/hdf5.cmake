0a4e27922118c323694dba6e564e64c5d9ff3037
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-11 21:46:08 -0500

    cmake: find hdf5.mod that may be missing from include_dirs
     find auxiliary libraries needed by hdf5 when compiler wrapper is not working

--- cmake/hdf5.cmake
+++ cmake/hdf5.cmake
@@ -1,21 +1,43 @@
 # don't enclose this all in "if(NOT DEFINED HDF5OK)" because CMake intermittantly doesn't cache needed HDF5 variables.
 
 set(HDF5_USE_STATIC_LIBRARIES true)
-
 # Intel HDF5 for Windows has some real issues from the factory, this makes it work:
 if(WIN32 AND CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
   set(HDF5_NO_FIND_PACKAGE_CONFIG_FILE true)
-  set(HDF5_SEARCH_WRAPPER false)
   set(HDF5_USE_STATIC_LIBRARIES false)
 endif()
 
 find_package(HDF5 REQUIRED COMPONENTS Fortran HL)
-if(MINGW)
-  set(HDF5_INCLUDE_DIRS ${HDF5_INCLUDE_DIRS}/static)
-endif()
-
-set(HDF5_LIBRARIES ${HDF5_Fortran_HL_LIBRARIES} ${HDF5_Fortran_LIBRARIES} ${HDF5_LIBRARIES})
 list(APPEND HDF5_INCLUDE_DIRS ${HDF5_Fortran_INCLUDE_DIRS})
+# this is because HDF5's CONFIG files are incorrect (at least for 1.10.5)
+find_path(HDF5_MODULE_DIR NAMES hdf5.mod HINTS ${HDF5_INCLUDE_DIRS} PATH_SUFFIXES static)
+if(HDF5_MODULE_DIR)
+  list(APPEND HDF5_INCLUDE_DIRS ${HDF5_MODULE_DIR})
+  set(SZIP_ROOT "${HDF5_MODULE_DIR}/..;${HDF5_MODULE_DIR}/../..")
+  set(ZLIB_ROOT ${SZIP_ROOT})
+endif()
+list(REMOVE_DUPLICATES HDF5_INCLUDE_DIRS)
+
+# in case the compiler wrapper didn't work
+set(HDF5_LIBRARIES ${HDF5_Fortran_HL_LIBRARIES} ${HDF5_Fortran_LIBRARIES} ${HDF5_LIBRARIES})
+
+find_package(SZIP)
+if(SZIP_FOUND)
+  list(APPEND HDF5_LIBRARIES SZIP::SZIP)
+endif()
+find_package(ZLIB)
+if(ZLIB_FOUND)
+  list(APPEND HDF5_LIBRARIES ZLIB::ZLIB)
+endif()
+set(THREADS_PREFER_PTHREAD_FLAG true)
+find_package(Threads)
+if(Threads_FOUND)
+  list(APPEND HDF5_LIBRARIES Threads::Threads)
+endif()
+list(APPEND HDF5_LIBRARIES ${CMAKE_DL_LIBS})
+if(UNIX)
+  list(APPEND HDF5_LIBRARIES m)
+endif()
 
 if(DEFINED HDF5OK)
   return()
@@ -23,6 +45,7 @@ endif()
 
 message(STATUS "HDF5 include: ${HDF5_INCLUDE_DIRS}")
 message(STATUS "HDF5 library: ${HDF5_LIBRARIES}")
+# we don't use these because they may come from a broken compiler wrapper
 if(HDF5_Fortran_COMPILER_EXECUTABLE)
   message(STATUS "HDF5 Fortran compiler: ${HDF5_Fortran_COMPILER_EXECUTABLE}")
 endif()
