91e475d62f3932847c2d011dd47c469113ff96fc
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-07-31 18:31:06 -0400

    cleanup example

---
+++ Examples/example1.f90
@@ -0,0 +1,18 @@
+program example1
+
+use h5fortran
+implicit none (type, external)
+
+character(1024) :: filename
+integer :: i32
+
+filename = '/tmp/h5fortran_example1.h5'
+
+call h5write(filename, '/x', 123)
+
+call h5read(filename, '/x', i32)
+if (i32 /= 123) error stop 'incorrect value read'
+
+print *, 'OK: example 1'
+
+end program
