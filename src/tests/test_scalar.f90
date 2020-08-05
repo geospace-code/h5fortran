2f526ddf1d3cdf35fb8142e88d0163d07f540e52
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-08-05 12:24:24 -0400

    add more casting tests: real<->int, scalar and array

--- src/tests/test_scalar.f90
+++ src/tests/test_scalar.f90
@@ -45,6 +45,13 @@ if (.not.(rt==it .and. it==42)) then
   error stop 'scalar real / int: not equal 42'
 endif
 
+!> read casting -- real to int and int to real
+call h%read('/scalar_real', it)
+if(it/=42) error stop 'scalar cast real => int'
+call h%read('/scalar_int', rt)
+if(rt/=42) error stop 'scalar cast int => real'
+
+!> 1D vector read write
 call h%shape('/1d_real',dims)
 allocate(rr1(dims(1)))
 call h%read('/1d_real',rr1)
@@ -54,6 +61,14 @@ call h%shape('/1d_int',dims)
 allocate(i1t(dims(1)))
 call h%read('/1d_int',i1t)
 if (.not.all(i1==i1t)) error stop 'integer 1-D: read does not match write'
+
+!> 1D vector read casting -- real to int and int to real
+call h%read('/1d_real', i1t)
+if (.not.all(r1==i1t)) error stop '1Dcast real => int'
+call h%read('/1d_int', rr1)
+if (.not.all(i1==rr1)) error stop '1D cast int => real'
+
+!> check filename property
 
 if (.not. h%filename == fn) then
   write(stderr,*) h%filename // ' mismatch filename'
