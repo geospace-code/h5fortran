a7c62bd8ba4dbfcc8beebeb1ece722de32c088c1
Michael Hirsch <scivision@users.noreply.github.com>
2020-09-15 14:23:54 -0400

    use HDF5_ROOT for self-built too

--- cmake/build_hdf5.cmake
+++ cmake/build_hdf5.cmake
@@ -1,7 +1,9 @@
 # builds HDF5 library from scratch
 # the if(NOT IS_DIRECTORY ..) statement is because otherwise CMake configures HDF5 each and every build cycle, which takes a minute or so.
 
-if(NOT IS_DIRECTORY ${hdf5_bindir}/lib)
+if(NOT IS_DIRECTORY ${HDF5_ROOT}/lib)
+
+  set(HDF5_ROOT ${PROJECT_BINARY_DIR}/hdf5 CACHE PATH "HDF5 library install location")
 
   include(ExternalProject)
   ExternalProject_Add(HDF5_proj
@@ -9,21 +11,21 @@ if(NOT IS_DIRECTORY ${hdf5_bindir}/lib)
   GIT_TAG 1.12/master
   GIT_SHALLOW true
   UPDATE_DISCONNECTED true
-  CONFIGURE_COMMAND ${CMAKE_CURRENT_BINARY_DIR}/HDF5_proj-prefix/src/HDF5_proj/configure --prefix=${hdf5_bindir} --enable-fortran --enable-build-mode=production --disable-tests --disable-tools --disable-shared
+  CONFIGURE_COMMAND ${CMAKE_CURRENT_BINARY_DIR}/HDF5_proj-prefix/src/HDF5_proj/configure --prefix=${HDF5_ROOT} --enable-fortran --enable-build-mode=production --disable-tests --disable-tools --disable-shared
   BUILD_COMMAND make -j
   INSTALL_COMMAND make -j install
   )
 
-  message(STATUS "installing HDF5 library in ${hdf5_bindir}")
+  message(STATUS "installing HDF5 library in ${HDF5_ROOT}")
 
-  file(MAKE_DIRECTORY ${hdf5_bindir}/include)  # avoid race condition
+  file(MAKE_DIRECTORY ${HDF5_ROOT}/include)  # avoid race condition
 
   add_dependencies(h5fortran HDF5_proj)  # ensure HDF5 builds first
 endif()
 
 set(HDF5_LIBRARIES)
 foreach(_l hdf5_hl_fortran hdf5_fortran hdf5_hl hdf5)
-  list(APPEND HDF5_LIBRARIES ${hdf5_bindir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}${_l}${CMAKE_STATIC_LIBRARY_SUFFIX})
+  list(APPEND HDF5_LIBRARIES ${HDF5_ROOT}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}${_l}${CMAKE_STATIC_LIBRARY_SUFFIX})
 endforeach()
 
 find_package(ZLIB REQUIRED)
@@ -46,6 +48,6 @@ message(VERBOSE " using self-built ${HDF5_LIBRARIES}")
 add_library(hdf5 INTERFACE IMPORTED)
 set_target_properties(hdf5 PROPERTIES
 INTERFACE_LINK_LIBRARIES "${HDF5_LIBRARIES}"
-INTERFACE_INCLUDE_DIRECTORIES ${hdf5_bindir}/include)
+INTERFACE_INCLUDE_DIRECTORIES ${HDF5_ROOT}/include)
 
 add_library(HDF5::HDF5 ALIAS hdf5)
