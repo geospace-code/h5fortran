df52920e68d66febb4f14785d0b645baf5fd3496
Michael Hirsch <scivision@users.noreply.github.com>
2020-09-30 03:35:54 -0400

    for linux, 2nd configure works

--- cmake/build_hdf5.cmake
+++ cmake/build_hdf5.cmake
@@ -21,7 +21,13 @@ file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/HDF5proj-prefix/src/HDF5proj-build/bin
 # --- Zlib
 # always build Zlib to ensure compatibility. This is a common practice for HDF5.
 
-set(_zlib_file ${PROJECT_BINARY_DIR}/ZLIBproj-prefix/src/ZLIBproj-build/${CMAKE_STATIC_LIBRARY_PREFIX}zlibstatic${CMAKE_STATIC_LIBRARY_SUFFIX})
+if(WIN32)
+  set(_zlib_name ${CMAKE_STATIC_LIBRARY_PREFIX}zlibstatic${CMAKE_STATIC_LIBRARY_SUFFIX})
+else()
+  set(_zlib_name ${CMAKE_STATIC_LIBRARY_PREFIX}z${CMAKE_STATIC_LIBRARY_SUFFIX})
+endif()
+
+set(_zlib_file ${PROJECT_BINARY_DIR}/ZLIBproj-prefix/src/ZLIBproj-build/${_zlib_name})
 set(_zlib_build ${PROJECT_BINARY_DIR}/ZLIBproj-prefix/src/ZLIBproj-build)
 set(_zlib_h ${PROJECT_BINARY_DIR}/ZLIBproj-prefix/src/ZLIBproj/zlib.h)
 
@@ -48,21 +54,26 @@ target_include_directories(ZLIB::ZLIB INTERFACE ${_zlib_build})
 set(_zlib_root -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON -DZLIB_ROOT:PATH=${_zlib_build} -DZLIB_LIBRARY:FILEPATH=${_zlib_file} -DZLIB_INCLUDE_DIR:PATH=${_zlib_build} -DZLIB_USE_EXTERNAL:BOOL=OFF)
 
 # --- HDF5
+# https://forum.hdfgroup.org/t/issues-when-using-hdf5-as-a-git-submodule-and-using-cmake-with-add-subdirectory/7189/2
 
-ExternalProject_Add(HDF5proj
-GIT_REPOSITORY https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git
-GIT_TAG 1.12/master
-GIT_SHALLOW true
-# URL https://hdf-wordpress-1.s3.amazonaws.com/wp-content/uploads/manual/HDF5/HDF5_1_12_0/source/CMake-hdf5-1.12.0.tar.gz
-# URL_HASH MD5=33ab3d5b9019ca468364d226e0ccdea6
-UPDATE_DISCONNECTED true
-CMAKE_ARGS ${_zlib_root} -DBUILD_SHARED_LIBS:BOOL=false -DCMAKE_BUILD_TYPE=Release -DHDF5_BUILD_FORTRAN:BOOL=true -DHDF5_BUILD_CPP_LIB:BOOL=false -DHDF5_BUILD_TOOLS:BOOL=false -DBUILD_TESTING:BOOL=false -DHDF5_BUILD_EXAMPLES:BOOL=false
-BUILD_BYPRODUCTS ${HDF5_LIBRARIES}
-INSTALL_COMMAND ""
-)
+if(HDF5OK)
+  add_custom_target(HDF5proj DEPENDS ${HDF5_LIBRARIES})
+  add_custom_command(OUTPUT ${HDF5_LIBRARIES})
+else()
+  ExternalProject_Add(HDF5proj
+  GIT_REPOSITORY https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git
+  GIT_TAG 1.12/master
+  GIT_SHALLOW true
+  # URL https://hdf-wordpress-1.s3.amazonaws.com/wp-content/uploads/manual/HDF5/HDF5_1_12_0/source/CMake-hdf5-1.12.0.tar.gz
+  # URL_HASH MD5=33ab3d5b9019ca468364d226e0ccdea6
+  UPDATE_DISCONNECTED true
+  CMAKE_ARGS ${_zlib_root} -DHDF5_GENERATE_HEADERS:BOOL=false -DHDF5_DISABLE_COMPILER_WARNINGS:BOOL=true -DBUILD_SHARED_LIBS:BOOL=false -DCMAKE_BUILD_TYPE=Release -DHDF5_BUILD_FORTRAN:BOOL=true -DHDF5_BUILD_CPP_LIB:BOOL=false -DHDF5_BUILD_TOOLS:BOOL=false -DBUILD_TESTING:BOOL=false -DHDF5_BUILD_EXAMPLES:BOOL=false
+  BUILD_BYPRODUCTS ${HDF5_LIBRARIES}
+  INSTALL_COMMAND ""
+  )
 
-add_dependencies(HDF5proj ZLIBproj)
-
+  add_dependencies(HDF5proj ZLIBproj)
+endif(HDF5OK)
 # --- external deps
 
 list(APPEND HDF5_LIBRARIES ZLIB::ZLIB)
@@ -81,6 +92,5 @@ endif(UNIX)
 
 # this GLOBAL is required to be visible via FetchContent
 add_library(HDF5::HDF5 INTERFACE IMPORTED GLOBAL)
-set_target_properties(HDF5::HDF5 PROPERTIES
-INTERFACE_LINK_LIBRARIES "${HDF5_LIBRARIES}"
-INTERFACE_INCLUDE_DIRECTORIES "${HDF5_INCLUDE_DIRS}")
+target_link_libraries(HDF5::HDF5 INTERFACE ${HDF5_LIBRARIES})
+target_include_directories(HDF5::HDF5 INTERFACE ${HDF5_INCLUDE_DIRS})
