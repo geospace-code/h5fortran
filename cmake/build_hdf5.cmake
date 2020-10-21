a0dc92643b45603110521830073c52af86669632
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-10-21 18:47:53 -0400

    put zlib in own file

--- cmake/build_hdf5.cmake
+++ cmake/build_hdf5.cmake
@@ -19,39 +19,10 @@ endif()
 file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/HDF5proj-prefix/src/HDF5proj-build/bin/static)  # avoid race condition
 
 # --- Zlib
-# always build Zlib to ensure compatibility. This is a common practice for HDF5.
-
-if(WIN32)
-  set(_zlib_name ${CMAKE_STATIC_LIBRARY_PREFIX}zlibstatic${CMAKE_STATIC_LIBRARY_SUFFIX})
-else()
-  set(_zlib_name ${CMAKE_STATIC_LIBRARY_PREFIX}z${CMAKE_STATIC_LIBRARY_SUFFIX})
+set(zlib_root -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON -DZLIB_USE_EXTERNAL:BOOL=OFF)
+if(NOT TARGET ZLIB::ZLIB)
+  include(${CMAKE_CURRENT_LIST_DIR}/zlib_setup.cmake)
 endif()
-
-set(_zlib_file ${PROJECT_BINARY_DIR}/ZLIBproj-prefix/src/ZLIBproj-build/${_zlib_name})
-set(_zlib_build ${PROJECT_BINARY_DIR}/ZLIBproj-prefix/src/ZLIBproj-build)
-set(_zlib_h ${PROJECT_BINARY_DIR}/ZLIBproj-prefix/src/ZLIBproj/zlib.h)
-
-ExternalProject_Add(ZLIBproj
-URL https://zlib.net/zlib1211.zip
-URL_HASH SHA1=bccd93ad3cee39c3d08eee68d45b3e11910299f2
-UPDATE_DISCONNECTED true
-CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release
-BUILD_BYPRODUCTS ${_zlib_file}
-INSTALL_COMMAND ""
-)
-
-if(NOT IS_DIRECTORY ${_zlib_build})
-  file(MAKE_DIRECTORY ${_zlib_build})  # avoid race condition
-endif()
-if(NOT EXISTS ${_zlib_build}/zlib.h)
-  file(CREATE_LINK ${_zlib_h} ${_zlib_build}/zlib.h SYMBOLIC)
-endif()
-
-add_library(ZLIB::ZLIB INTERFACE IMPORTED GLOBAL)
-target_link_libraries(ZLIB::ZLIB INTERFACE ${_zlib_file})
-target_include_directories(ZLIB::ZLIB INTERFACE ${_zlib_build})
-
-set(_zlib_root -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON -DZLIB_ROOT:PATH=${_zlib_build} -DZLIB_LIBRARY:FILEPATH=${_zlib_file} -DZLIB_INCLUDE_DIR:PATH=${_zlib_build} -DZLIB_USE_EXTERNAL:BOOL=OFF)
 
 # --- HDF5
 # https://forum.hdfgroup.org/t/issues-when-using-hdf5-as-a-git-submodule-and-using-cmake-with-add-subdirectory/7189/2
@@ -68,12 +39,14 @@ else()
   # URL https://hdf-wordpress-1.s3.amazonaws.com/wp-content/uploads/manual/HDF5/HDF5_1_12_0/source/CMake-hdf5-1.12.0.tar.gz
   # URL_HASH MD5=33ab3d5b9019ca468364d226e0ccdea6
   UPDATE_DISCONNECTED true
-  CMAKE_ARGS ${_zlib_root} -DHDF5_GENERATE_HEADERS:BOOL=false -DHDF5_DISABLE_COMPILER_WARNINGS:BOOL=true -DBUILD_SHARED_LIBS:BOOL=false -DCMAKE_BUILD_TYPE=Release -DHDF5_BUILD_FORTRAN:BOOL=true -DHDF5_BUILD_CPP_LIB:BOOL=false -DHDF5_BUILD_TOOLS:BOOL=false -DBUILD_TESTING:BOOL=false -DHDF5_BUILD_EXAMPLES:BOOL=false
+  CMAKE_ARGS ${zlib_root} -DHDF5_GENERATE_HEADERS:BOOL=false -DHDF5_DISABLE_COMPILER_WARNINGS:BOOL=true -DBUILD_SHARED_LIBS:BOOL=false -DCMAKE_BUILD_TYPE=Release -DHDF5_BUILD_FORTRAN:BOOL=true -DHDF5_BUILD_CPP_LIB:BOOL=false -DHDF5_BUILD_TOOLS:BOOL=false -DBUILD_TESTING:BOOL=false -DHDF5_BUILD_EXAMPLES:BOOL=false
   BUILD_BYPRODUCTS ${HDF5_LIBRARIES}
   INSTALL_COMMAND ""
   )
 
-  add_dependencies(HDF5proj ZLIBproj)
+  if(NOT TARGET ZLIB::ZLIB)
+    add_dependencies(HDF5proj ZLIBproj)
+  endif()
 endif(HDF5OK)
 # --- external deps
 
---
+++ cmake/zlib_setup.cmake
@@ -0,0 +1,33 @@
+# build Zlib to ensure compatibility. This is a common practice for HDF5.
+
+if(WIN32)
+  set(_zlib_name ${CMAKE_STATIC_LIBRARY_PREFIX}zlibstatic${CMAKE_STATIC_LIBRARY_SUFFIX})
+else()
+  set(_zlib_name ${CMAKE_STATIC_LIBRARY_PREFIX}z${CMAKE_STATIC_LIBRARY_SUFFIX})
+endif()
+
+set(_zlib_file ${PROJECT_BINARY_DIR}/ZLIBproj-prefix/src/ZLIBproj-build/${_zlib_name})
+set(_zlib_build ${PROJECT_BINARY_DIR}/ZLIBproj-prefix/src/ZLIBproj-build)
+set(_zlib_h ${PROJECT_BINARY_DIR}/ZLIBproj-prefix/src/ZLIBproj/zlib.h)
+
+ExternalProject_Add(ZLIBproj
+URL https://zlib.net/zlib1211.zip
+URL_HASH SHA1=bccd93ad3cee39c3d08eee68d45b3e11910299f2
+UPDATE_DISCONNECTED true
+CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release
+BUILD_BYPRODUCTS ${_zlib_file}
+INSTALL_COMMAND ""
+)
+
+if(NOT IS_DIRECTORY ${_zlib_build})
+  file(MAKE_DIRECTORY ${_zlib_build})  # avoid race condition
+endif()
+if(NOT EXISTS ${_zlib_build}/zlib.h)
+  file(CREATE_LINK ${_zlib_h} ${_zlib_build}/zlib.h SYMBOLIC)
+endif()
+
+add_library(ZLIB::ZLIB INTERFACE IMPORTED GLOBAL)
+target_link_libraries(ZLIB::ZLIB INTERFACE ${_zlib_file})
+target_include_directories(ZLIB::ZLIB INTERFACE ${_zlib_build})
+
+list(APPEND zlib_root -DZLIB_ROOT:PATH=${_zlib_build} -DZLIB_LIBRARY:FILEPATH=${_zlib_file} -DZLIB_INCLUDE_DIR:PATH=${_zlib_build})
