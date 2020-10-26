f33c7a19c8f7e82499e249b4feeb0f43ee32b04e
Michael Hirsch <scivision@users.noreply.github.com>
2020-10-26 17:19:58 -0400

    dedupe hdf5_external specification

--- cmake/build_hdf5.cmake
+++ cmake/build_hdf5.cmake
@@ -2,6 +2,8 @@
 # note: the use of "lib" vs. CMAKE_STATIC_LIBRARY_PREFIX is deliberate based on the particulars of these libraries
 # across Intel Fortran on Windows vs. Gfortran on Windows vs. Linux.
 
+set(hdf5_external true CACHE BOOL "autobuild HDF5")
+
 include(ExternalProject)
 
 set(HDF5_LIBRARIES)
