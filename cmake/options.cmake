ada74580f109e1453b0bc501b5ef81c8a8947a86
Michael Hirsch <scivision@users.noreply.github.com>
2021-02-22 09:52:49 -0500

    auto-ignore build dir

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -9,3 +9,8 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS on)
 if(NOT dev)
   set(EP_UPDATE_DISCONNECTED true)
 endif()
+
+# --- auto-ignore build directory
+if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
+  file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")
+endif()
