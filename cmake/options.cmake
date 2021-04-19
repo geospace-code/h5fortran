b71e618859488136241a19f4a09388a688a5ea38
Michael Hirsch <scivision@users.noreply.github.com>
2021-04-19 00:56:25 -0400

    shaky tests off by default

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,13 +2,13 @@
 option(hdf5_external "Build HDF5 library")
 option(dev "developer mode")
 
+option(shaky "run shaky tests that may segfault since designed to fail" off)
 option(matlab "check HDF5 file writes with Matlab" off)
 option(concepts "conceptual testing, for devs only" off)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS on)
 
 if(dev)
-
 else()
   set(EP_UPDATE_DISCONNECTED true)
 endif()
