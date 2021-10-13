b21931018c85b4d3eba71b89ebbd96139187b45c
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-13 13:17:51 -0400

    intel: correct xHost

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -16,7 +16,8 @@ endif()
 # we left off "-std=f2018" type flags as they tend to issue false warnings
 
 add_compile_options(
-"$<$<COMPILE_LANG_AND_ID:Fortran,Intel,IntelLLVM>:-xHost;-warn;-heap-arrays>"
+$<$<C_COMPILER_ID:Intel,IntelLLVM>:$<IF:$<BOOL:${WIN32}>,/QxHost,-xHost>>
+"$<$<COMPILE_LANG_AND_ID:Fortran,Intel,IntelLLVM>:-warn;-heap-arrays>"
 "$<$<AND:$<COMPILE_LANG_AND_ID:Fortran,Intel,IntelLLVM>,$<CONFIG:Debug>>:-traceback;-check;-debug>"
 "$<$<COMPILE_LANG_AND_ID:Fortran,GNU>:-mtune=native;-Wall;-fimplicit-none>"
 "$<$<AND:$<COMPILE_LANG_AND_ID:Fortran,GNU>,$<CONFIG:Debug>>:-Wextra;-fcheck=all;-Werror=array-bounds>"
