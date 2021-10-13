483fe72c8ba96c93c7b0dedf7bf59baf78e77a2e
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-13 13:10:31 -0400

    intel option no space

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -17,7 +17,7 @@ endif()
 
 add_compile_options(
 "$<$<COMPILE_LANG_AND_ID:Fortran,Intel,IntelLLVM>:-xHost;-warn;-heap-arrays>"
-"$<$<AND:$<COMPILE_LANG_AND_ID:Fortran,Intel,IntelLLVM>,$<CONFIG:Debug>>:-traceback;-check all;-debug extended>"
+"$<$<AND:$<COMPILE_LANG_AND_ID:Fortran,Intel,IntelLLVM>,$<CONFIG:Debug>>:-traceback;-check;-debug>"
 "$<$<COMPILE_LANG_AND_ID:Fortran,GNU>:-mtune=native;-Wall;-fimplicit-none>"
 "$<$<AND:$<COMPILE_LANG_AND_ID:Fortran,GNU>,$<CONFIG:Debug>>:-Wextra;-fcheck=all;-Werror=array-bounds>"
 "$<$<AND:$<COMPILE_LANG_AND_ID:Fortran,GNU>,$<CONFIG:Release>>:-fno-backtrace;-Wno-maybe-uninitialized>"
