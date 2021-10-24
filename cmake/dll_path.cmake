3e3a7d9e8ba1284b6fe5724e2a1bd5b89da173ab
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-24 17:08:31 -0400

    CMake < 3.22 windows DLL path

--- cmake/dll_path.cmake
+++ cmake/dll_path.cmake
@@ -1,32 +1,30 @@
 # Windows executables (Intel oneAPI, MinGW GCC, etc.) need DLL's on PATH.
-# NOTE: CMake 3.22 added test property ENVIRONMENT_MODIFICATION that may do this more smoothly:
-# https://cmake.org/cmake/help/git-stage/prop_test/ENVIRONMENT_MODIFICATION.html
+# FIXME: when CMake 3.22 released, can use ENVIRONMENT_MODIFICATION
 
-set(dll_path)
+set(DLL_PATH)
 
 if(NOT (MSVC OR (MINGW AND BUILD_SHARED_LIBS)))
   return()
 endif()
 
 if(hdf5_external)
-  set(ZLIB_DLL_DIR ${ZLIB_ROOT}/bin)
+  set(ZLIB_DLL_PATH ${ZLIB_ROOT}/bin)
 else()
-  find_path(ZLIB_DLL_DIR
+  find_path(ZLIB_DLL_PATH
   NAMES zlib.dll zlib1.dll libzlib1.dll
   NO_DEFAULT_PATH
-  HINTS ${ZLIB_INCLUDE_DIR}/.. ${ZLIB_ROOT} ENV ZLIB_ROOT
+  HINTS ${ZLIB_INCLUDE_PATH}/.. ${ZLIB_ROOT} ENV ZLIB_ROOT
   PATH_SUFFIXES bin
   DOC "DLL PATH"
   )
 endif()
 
-if(NOT ZLIB_DLL_DIR)
-  message(STATUS "Could not find DLL path, tests may fail.")
+if(NOT ZLIB_DLL_PATH)
+  message(STATUS "Could not find DLL path, tests may fail to run.")
   return()
 endif()
 
-set(dll_path "${ZLIB_DLL_DIR};$ENV{PATH}")
-cmake_path(CONVERT "${dll_path}" TO_NATIVE_PATH_LIST dll_path NORMALIZE)
+cmake_path(CONVERT "${ZLIB_DLL_PATH};$ENV{PATH}" TO_NATIVE_PATH_LIST DLL_PATH NORMALIZE)
 
 # this is the vital line, without it CMake set_tests_properties mangles the ENVIRONMENT
-string(REPLACE ";" "\\;" dll_path "${dll_path}")
+string(REPLACE ";" "\\;" DLL_PATH "${DLL_PATH}")
