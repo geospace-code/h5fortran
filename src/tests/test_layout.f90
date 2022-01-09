83c4b63dfe1f53efa692db9e1a22e2fa1267b77d
Michael Hirsch <scivision@users.noreply.github.com>
2022-01-09 08:58:25 -0500

    test_layout: functionalize

--- src/tests/test_layout.f90
+++ src/tests/test_layout.f90
@@ -1,27 +1,36 @@
 program test_layout
 
-use, intrinsic :: iso_fortran_env, only : real32, real64
+use, intrinsic :: iso_fortran_env, only : real32, real64, int32
 use h5fortran, only : hdf5_file
 use hdf5, only : H5D_COMPACT_F
 
 implicit none (type, external)
 
-type(hdf5_file) :: h
 character(*), parameter :: fn = 'test_layout.h5'
 
-real(real32), dimension(1,1,1,1,1,1,1) :: d7_32, r7_32
-real(real64), dimension(1,1,1,1,1,1,1) :: d7_64, r7_64
+call test_layout_write(fn)
 
-real(real32) :: w32, r32
-real(real64) :: w64, r64
-integer :: wi32, ri32
+call test_layout_read(fn)
+
+call test_layout_props(fn)
+
+print *, "OK: test layout"
+
+contains
+
+
+subroutine test_layout_write(fn)
+
+character(*), intent(in) :: fn
+
+type(hdf5_file) :: h
+
+real(real32), dimension(1,1,1,1,1,1,1) :: d7_32
+real(real64), dimension(1,1,1,1,1,1,1) :: d7_64
 
 d7_32 = 42
 d7_64 = 42
 
-w32 = 142
-w64 = 142
-wi32 = 142
 
 call h%open(fn, action="w")
 
@@ -32,29 +41,57 @@ call h%write("/compact0d", 42, compact=.true.)
 call h%write("/compact7d_32", d7_32, compact=.true.)
 call h%write("/compact7d_64", d7_64, compact=.true.)
 
-call h%write('/compact_r32', w32, compact=.true.)
-call h%write('/compact_r64', w64, compact=.true.)
-call h%write('/compact_i32', wi32, compact=.true.)
+call h%write('/compact_r32', 142._real32, compact=.true.)
+call h%write('/compact_r64', 142._real64, compact=.true.)
+call h%write('/compact_i32', 142_int32, compact=.true.)
 
 call h%close()
+
+end subroutine test_layout_write
+
+
+subroutine test_layout_read(fn)
+
+character(*), intent(in) :: fn
+
+type(hdf5_file) :: h
+
+real(real32) :: r32
+real(real64) :: r64
+integer(int32) :: ri32
+
+real(real32), dimension(1,1,1,1,1,1,1) :: r7_32
+real(real64), dimension(1,1,1,1,1,1,1) :: r7_64
 
 !> read casting
 call h%open(fn, action="r")
 
 call h%read("/compact_r32", r64)
-if(r64 /= w32) error stop "read real32 => real64"
+if(r64 /= 142) error stop "read real32 => real64"
 call h%read("/compact_r64", r32)
-if(r32 /= w64) error stop "read real64 => real32"
+if(r32 /= 142) error stop "read real64 => real32"
 call h%read("/compact_i32", ri32)
-if(r32 /= wi32) error stop "read int32 => int32"
+if(r32 /= 142) error stop "read int32 => int32"
 
 
 call h%read("/compact7d_32", r7_64)
-if (any(r7_64 /= d7_32)) error stop "read real32 => real64"
+if (any(r7_64 /= 42)) error stop "read real32 => real64"
 
 call h%read("/compact7d_64", r7_32)
-if (any(r7_32 /= d7_64)) error stop "read real64 => real32"
+if (any(r7_32 /= 42)) error stop "read real64 => real32"
 
+call h%close()
+
+end subroutine test_layout_read
+
+
+subroutine test_layout_props(fn)
+
+character(*), intent(in) :: fn
+
+type(hdf5_file) :: h
+
+call h%open(fn, action="r")
 
 if (h%layout("/compact1d") /= H5D_COMPACT_F) error stop "expected compact"
 if (.not. h%is_compact("/compact1d")) error stop "1d is_compact fail"
@@ -64,5 +101,6 @@ if (.not. h%is_compact("/compact0d")) error stop "0d is_compact fail"
 
 call h%close()
 
+end subroutine test_layout_props
 
 end program
