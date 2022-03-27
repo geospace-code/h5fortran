5dd2df555b08b540b4a810182e10edc6dcb13805
Michael Hirsch <scivision@users.noreply.github.com>
2022-03-27 15:28:25 -0400

    FindHDF5: szip/zlib: require if config'd and HDF5 REQUIRED

--- cmake/Modules/FindSZIP.cmake
+++ cmake/Modules/FindSZIP.cmake
@@ -27,24 +27,28 @@ Targets
   SZIP Imported Target
 #]=======================================================================]
 
-find_package(libaec QUIET)
+find_package(libaec CONFIG QUIET)
+# libaec sets SZIP_FOUND, SZIP_LIBRARIES, SZIP_INCLUDE_DIR
 
 if(NOT SZIP_FOUND)
 
   find_library(SZIP_LIBRARY
-    NAMES szip sz
-    NAMES_PER_DIR
-    DOC "SZIP API")
+  NAMES szip sz
+  NAMES_PER_DIR
+  DOC "SZIP API"
+  )
 
   find_path(SZIP_INCLUDE_DIR
-    NAMES szlib.h
-    DOC "SZIP header")
+  NAMES szlib.h
+  DOC "SZIP header"
+  )
 
 endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SZIP
-  REQUIRED_VARS SZIP_LIBRARY SZIP_INCLUDE_DIR)
+REQUIRED_VARS SZIP_LIBRARY SZIP_INCLUDE_DIR
+)
 
 if(SZIP_FOUND)
   set(SZIP_INCLUDE_DIRS ${SZIP_INCLUDE_DIR})
@@ -53,8 +57,9 @@ if(SZIP_FOUND)
   if(NOT TARGET SZIP::SZIP)
     add_library(SZIP::SZIP INTERFACE IMPORTED)
     set_target_properties(SZIP::SZIP PROPERTIES
-      INTERFACE_LINK_LIBRARIES "${SZIP_LIBRARIES}"
-      INTERFACE_INCLUDE_DIRECTORIES "${SZIP_INCLUDE_DIRS}")
+    INTERFACE_LINK_LIBRARIES "${SZIP_LIBRARIES}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SZIP_INCLUDE_DIRS}"
+    )
   endif()
 endif()
 
