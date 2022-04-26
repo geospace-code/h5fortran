85c88531558e24856f0d80ed1167a31d0d55a4f5
Michael Hirsch <scivision@users.noreply.github.com>
2022-04-26 17:37:24 -0400

    ci.cmake: simplify [skip ci]

--- ci.cmake
+++ ci.cmake
@@ -8,22 +8,15 @@ set(opts)
 
 # --- boilerplate follows
 
-set(CI)
-if(DEFINED ENV{CI})
+if(NOT DEFINED CI AND DEFINED ENV{CI})
   set(CI $ENV{CI})
 endif()
 
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 set(CTEST_SUBMIT_URL "https://my.cdash.org/submit.php?project=${CTEST_PROJECT_NAME}")
 
-# ctest -S doesn't have a way to pass -Dvar:type=value, so do this via env var
-# list(APPEND opts $ENV{CTEST_${CTEST_PROJECT_NAME}_ARGS})
-
 # --- Experimental, Nightly, Continuous
 # https://cmake.org/cmake/help/latest/manual/ctest.1.html#dashboard-client-modes
-if(NOT CTEST_MODEL AND DEFINED ENV{CTEST_MODEL})
-  set(CTEST_MODEL $ENV{CTEST_MODEL})
-endif()
 if(NOT CTEST_MODEL AND CI)
   set(CTEST_MODEL "Nightly")
 endif()
@@ -42,10 +35,8 @@ if(NOT DEFINED CTEST_BINARY_DIRECTORY)
   set(CTEST_BINARY_DIRECTORY ${CTEST_SOURCE_DIRECTORY}/build)
 endif()
 
-if(CTEST_MODEL MATCHES "(Nightly|Continuous)")
-  if(EXISTS ${CTEST_BINARY_DIRECTORY}/CMakeCache.txt)
-    ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
-  endif()
+if(EXISTS ${CTEST_BINARY_DIRECTORY}/CMakeCache.txt)
+  ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 endif()
 
 if(NOT DEFINED CTEST_SITE)
@@ -60,29 +51,16 @@ endif()
 
 find_program(GIT_EXECUTABLE NAMES git REQUIRED)
 
-if(NOT DEFINED CTEST_BUILD_NAME)
-  if(DEFINED ENV{CTEST_BUILD_NAME})
-    set(CTEST_BUILD_NAME $ENV{CTEST_BUILD_NAME})
-  else()
-    execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags
-    WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
-    OUTPUT_VARIABLE git_rev OUTPUT_STRIP_TRAILING_WHITESPACE
-    RESULT_VARIABLE _err
-    )
-    if(NOT _err EQUAL 0)
-      # old Git
-      execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
-      WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
-      OUTPUT_VARIABLE git_rev OUTPUT_STRIP_TRAILING_WHITESPACE
-      RESULT_VARIABLE _err
-      )
-    endif()
-    if(_err EQUAL 0)
-      set(CTEST_BUILD_NAME ${git_rev})
-    endif()
-  endif()
-endif()
+# --- CTEST_BUILD_NAME is used by ctest_submit(); must be set before ctest_start()
 
+if(NOT CTEST_BUILD_NAME)
+  execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags
+  WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
+  OUTPUT_VARIABLE git_rev OUTPUT_STRIP_TRAILING_WHITESPACE
+  COMMAND_ERROR_IS_FATAL ANY
+  )
+  set(CTEST_BUILD_NAME ${git_rev})
+endif()
 
 # --- find generator
 function(find_generator)
@@ -99,24 +77,21 @@ find_program(ninja NAMES ninja ninja-build samu)
 if(ninja)
   execute_process(COMMAND ${ninja} --version
   OUTPUT_VARIABLE ninja_version OUTPUT_STRIP_TRAILING_WHITESPACE
-  RESULT_VARIABLE err
+  RESULT_VARIABLE ret
   TIMEOUT 5
   )
-  if(err EQUAL 0 AND ninja_version VERSION_GREATER_EQUAL 1.10)
-    set(CTEST_CMAKE_GENERATOR Ninja)
+  if(ret EQUAL 0 AND ninja_version VERSION_GREATER_EQUAL 1.10)
+    set(CTEST_CMAKE_GENERATOR Ninja PARENT_SCOPE)
+    return()
   endif()
 endif(ninja)
 
-if(NOT CTEST_CMAKE_GENERATOR)
-  set(CTEST_BUILD_FLAGS -j)  # not --parallel as this goes to generator directly
-  if(WIN32)
-    set(CTEST_CMAKE_GENERATOR "MinGW Makefiles")
-  else()
-    set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-  endif()
+set(CTEST_BUILD_FLAGS -j)  # not --parallel as this goes to generator directly
+if(WIN32)
+  set(CTEST_CMAKE_GENERATOR "MinGW Makefiles" PARENT_SCOPE)
+else()
+  set(CTEST_CMAKE_GENERATOR "Unix Makefiles" PARENT_SCOPE)
 endif()
-
-set(CTEST_CMAKE_GENERATOR ${CTEST_CMAKE_GENERATOR} PARENT_SCOPE)
 
 endfunction(find_generator)
 
@@ -130,9 +105,6 @@ set(CTEST_UPDATE_TYPE git)
 set(CTEST_UPDATE_COMMAND git)
 
 ctest_start(${CTEST_MODEL})
-if(CI)
-  ctest_submit(PARTS Start)
-endif(CI)
 
 if(CTEST_MODEL MATCHES "(Nightly|Continuous)")
   # this erases local code changes i.e. anything not "git push" already is lost forever!
@@ -140,49 +112,59 @@ if(CTEST_MODEL MATCHES "(Nightly|Continuous)")
   execute_process(COMMAND ${GIT_EXECUTABLE} status --porcelain
   WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
   TIMEOUT 5
-  OUTPUT_VARIABLE _ret OUTPUT_STRIP_TRAILING_WHITESPACE
+  OUTPUT_VARIABLE ret OUTPUT_STRIP_TRAILING_WHITESPACE
   COMMAND_ERROR_IS_FATAL ANY
   )
-  if(_ret)
-    message(WARNING "CTest would have erased the non-Git Push'd changes, NOT updating.")
+  if(ret)
+    message(FATAL_ERROR "CTest would have erased the non-Git Push'd changes.")
   else()
-    ctest_update(RETURN_VALUE _ret)
-    if(_ret EQUAL 0 AND CTEST_MODEL STREQUAL Continuous)
-      message(STATUS "No Git-updated files, so no need to test in CTest Model ${CTEST_MODEL}. CTest stopping.")
+    ctest_update(
+    RETURN_VALUE ret
+    CAPTURE_CMAKE_ERROR err
+    )
+    if(ret LESS 0 OR NOT err EQUAL 0)
+      message(FATAL_ERROR "Update failed: return ${ret} cmake return ${err}")
+    endif()
+    if(ret EQUAL 0 AND CTEST_MODEL STREQUAL Continuous)
+      message(NOTICE "No Git-updated files -> no need to test in CTest Model ${CTEST_MODEL}. CTest stopping.")
       return()
     endif()
+
   endif()
 endif()
 
+# --- configure
+
 ctest_configure(
 OPTIONS "${opts}"
-RETURN_VALUE _ret
-CAPTURE_CMAKE_ERROR _err
+RETURN_VALUE ret
+CAPTURE_CMAKE_ERROR err
 )
-if(NOT (_ret EQUAL 0 AND _err EQUAL 0))
+if(NOT (ret EQUAL 0 AND err EQUAL 0))
   ctest_submit(BUILD_ID build_id)
-  message(FATAL_ERROR "Configure ${build_id} failed: return ${_ret} cmake return ${_err}")
+  message(FATAL_ERROR "Configure ${build_id} failed: return ${ret} cmake return ${err}")
 endif()
 
 ctest_build(
-RETURN_VALUE _ret
-CAPTURE_CMAKE_ERROR _err
+RETURN_VALUE ret
+CAPTURE_CMAKE_ERROR err
 )
-if(NOT (_ret EQUAL 0 AND _err EQUAL 0))
-  ctest_submit(BUILD_ID build_id)
-  message(FATAL_ERROR "Build ${build_id} failed: return ${_ret} cmake return ${_err}")
+ctest_submit(BUILD_ID build_id)
+if(NOT (ret EQUAL 0 AND err EQUAL 0))
+  message(FATAL_ERROR "Build ${build_id} failed: return ${ret} cmake return ${err}")
 endif()
 
 ctest_test(
 SCHEDULE_RANDOM ON
-RETURN_VALUE _ret
-CAPTURE_CMAKE_ERROR _err
+OUTPUT_JUNIT ${CTEST_BINARY_DIRECTORY}/ctest_${build_id}.json
+RETURN_VALUE ret
+CAPTURE_CMAKE_ERROR err
 )
 
 ctest_submit(BUILD_ID build_id)
 
-if(NOT (_ret EQUAL 0 AND _err EQUAL 0))
-  message(FATAL_ERROR "Test ${build_id} failed: CTest code ${_ret}, CMake code ${_err}.")
+if(NOT (ret EQUAL 0 AND err EQUAL 0))
+  message(FATAL_ERROR "Test ${build_id} failed: CTest code ${ret}, CMake code ${err}.")
 endif()
 
 message(STATUS "OK: CTest build ${build_id}")
