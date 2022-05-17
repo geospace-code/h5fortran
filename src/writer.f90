fe21f8589bb571d691cf36d9d4525c4f86ae0a1a
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-17 18:07:13 -0400

    writer template: use fortran standard include

--- src/writer.f90
+++ src/writer.f90
@@ -7,31 +7,31 @@ implicit none (type, external)
 contains
 
 module procedure h5write_1d
-@writer_template@
+include "writer_template.inc"
 end procedure h5write_1d
 
 module procedure h5write_2d
-@writer_template@
+include "writer_template.inc"
 end procedure h5write_2d
 
 module procedure h5write_3d
-@writer_template@
+include "writer_template.inc"
 end procedure h5write_3d
 
 module procedure h5write_4d
-@writer_template@
+include "writer_template.inc"
 end procedure h5write_4d
 
 module procedure h5write_5d
-@writer_template@
+include "writer_template.inc"
 end procedure h5write_5d
 
 module procedure h5write_6d
-@writer_template@
+include "writer_template.inc"
 end procedure h5write_6d
 
 module procedure h5write_7d
-@writer_template@
+include "writer_template.inc"
 end procedure h5write_7d
 
 end submodule writer
--- src/writer_lt.f90
+++ src/writer_lt.f90
@@ -6,35 +6,35 @@ contains
 
 
 module procedure lt0write
-@writer_lt_template@
+include "writer_lt_template.inc"
 end procedure lt0write
 
 module procedure lt1write
-@writer_lt_template@
+include "writer_lt_template.inc"
 end procedure lt1write
 
 module procedure lt2write
-@writer_lt_template@
+include "writer_lt_template.inc"
 end procedure lt2write
 
 module procedure lt3write
-@writer_lt_template@
+include "writer_lt_template.inc"
 end procedure lt3write
 
 module procedure lt4write
-@writer_lt_template@
+include "writer_lt_template.inc"
 end procedure lt4write
 
 module procedure lt5write
-@writer_lt_template@
+include "writer_lt_template.inc"
 end procedure lt5write
 
 module procedure lt6write
-@writer_lt_template@
+include "writer_lt_template.inc"
 end procedure lt6write
 
 module procedure lt7write
-@writer_lt_template@
+include "writer_lt_template.inc"
 end procedure lt7write
 
 end submodule writer_lt
