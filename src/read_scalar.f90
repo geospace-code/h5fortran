8d99306ece5cefffd62d119d4bd311e77cbcef8e
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-24 15:09:35 -0400

    read_scalar character: use h5dread

--- src/read_scalar.f90
+++ src/read_scalar.f90
@@ -1,6 +1,5 @@
 submodule (h5fortran:hdf5_read) read_scalar
 
-use h5lt, only : h5ltread_dataset_string_f
 use hdf5, only : h5dread_f, h5dget_space_f, h5dvlen_get_max_len_f, h5dread_vl_f, h5dvlen_reclaim_f,&
 h5tis_variable_str_f, &
 h5sclose_f, &
@@ -40,14 +39,16 @@ if(vector_scalar) then
   type is (integer(int64))
     call h5read_1d(self, dname, buf_i64)
     value = buf_i64(1)
+  type is(character(*))
+    error stop "ERROR:h5fortran:read_scalar:vector_scalar: character not yet supported for vector_scalar read"
   class default
-    error stop "h5fortran:read:vector_scalar: unknown memory variable type" // dname
+    error stop "ERROR:h5fortran:read_scalar:vector_scalar: unknown memory variable type" // dname
   end select
   return
 endif
 
 call h5dopen_f(self%file_id, dname, dset_id, ier)
-if(ier/=0) error stop 'h5fortran:reader: ' // dname // ' could not be opened in ' // self%filename
+if(ier/=0) error stop 'ERROR:h5fortran:reader: ' // dname // ' could not be opened in ' // self%filename
 
 call get_dset_class(self, dname, dclass, dset_id)
 
@@ -62,7 +63,7 @@ if(dclass == H5T_FLOAT_F) then
   type is (real(real32))
     call h5dread_f(dset_id, H5T_NATIVE_REAL, value, dims, ier)
   class default
-    error stop 'h5fortran:read: real disk dataset ' // dname // ' needs real memory variable'
+    error stop 'ERROR:h5fortran:read: real disk dataset ' // dname // ' needs real memory variable'
   end select
 elseif(dclass == H5T_INTEGER_F) then
   select type(value)
@@ -71,19 +72,19 @@ elseif(dclass == H5T_INTEGER_F) then
   type is (integer(int64))
     call h5dread_f(dset_id, H5T_STD_I64LE, value, dims, ier)
   class default
-    error stop 'h5fortran:read: integer disk dataset ' // dname // ' needs integer memory variable'
+    error stop 'ERROR:h5fortran:read: integer disk dataset ' // dname // ' needs integer memory variable'
   end select
 elseif(dclass == H5T_STRING_F) then
   select type(value)
   type is (character(*))
     call H5Dget_type_f(dset_id, type_id, ier)
-    if(ier/=0) error stop "h5fortran:read:h5tget_type " // dname // " in " // self%filename
+    if(ier/=0) error stop "ERROR:h5fortran:read:h5tget_type " // dname // " in " // self%filename
     call h5tis_variable_str_f(type_id, vstatus, ier)
-    if(ier/=0) error stop "h5fortran:read:h5tis_variable_str " // dname // " in " // self%filename
+    if(ier/=0) error stop "ERROR:h5fortran:read:h5tis_variable_str " // dname // " in " // self%filename
 
     if(vstatus) then
       call H5Dget_space_f(dset_id, space_id, ier)
-      if(ier/=0) error stop "h5fortran:read:h5dget_space " // dname // " in " // self%filename
+      if(ier/=0) error stop "ERROR:h5fortran:read:h5dget_space " // dname // " in " // self%filename
       !call h5dvlen_get_max_len_f(dset_id, type_id, space_id, dsize, ier)
       !if(ier/=0) error stop "h5fortran:read:h5dvlen_get_max_len " // dname // " in " // self%filename
 
@@ -96,7 +97,7 @@ elseif(dclass == H5T_STRING_F) then
       vldims = [len(buf_char), 1]
 
       call h5dread_vl_f(dset_id, type_id, buf_char, vldims, vlen, hdferr=ier, mem_space_id=space_id)
-      if(ier/=0) error stop "h5fortran:read:h5dread_vl " // dname // " in " // self%filename
+      if(ier/=0) error stop "ERROR:h5fortran:read:h5dread_vl " // dname // " in " // self%filename
 
       i = index(buf_char(1), c_null_char) - 1
       if (i == -1) i = len_trim(buf_char(1))
@@ -107,16 +108,16 @@ elseif(dclass == H5T_STRING_F) then
       end block
 
       call h5sclose_f(space_id, ier)
-      if(ier/=0) error stop "h5fortran:read:h5sclose " // dname // " in " // self%filename
+      if(ier/=0) error stop "ERROR:h5fortran:read:h5sclose " // dname // " in " // self%filename
     else
       call H5Tget_strpad_f(type_id, pad_type, ier)
-      if(ier/=0) error stop "h5fortran:read:h5tget_strpad " // dname // " in " // self%filename
+      if(ier/=0) error stop "ERROR:h5fortran:read:h5tget_strpad " // dname // " in " // self%filename
 
       call H5Tget_size_f(type_id, dsize, ier) !< only for non-variable
-      if(ier/=0) error stop "h5fortran:read:h5tget_size " // dname // " in " // self%filename
+      if(ier/=0) error stop "ERROR:h5fortran:read:h5tget_size " // dname // " in " // self%filename
 
       if(dsize > len(value)) then
-        write(stderr,'(a,i0,a3,i0,1x,a)') "h5fortran:read:string: buffer too small: ", dsize, " > ", len(value), &
+        write(stderr,'(a,i0,a3,i0,1x,a)') "ERROR:h5fortran:read:string: buffer too small: ", dsize, " > ", len(value), &
             dname // " in " // self%filename
         error stop
       endif
@@ -124,8 +125,8 @@ elseif(dclass == H5T_STRING_F) then
       block
       character(dsize) :: buf_char
 
-      call h5ltread_dataset_string_f(self%file_id, dname, buf_char, ier)
-      if(ier/=0) error stop "h5fortran:read:h5l5read_dataset_string " // dname // " in " // self%filename
+      call H5Dread_f(dset_id, type_id, buf_char, dims, ier)
+      if(ier/=0) error stop "ERROR:h5fortran:read:h5dread character " // dname // " in " // self%filename
 
       i = index(buf_char, c_null_char) - 1
       if (i == -1) i = len_trim(buf_char)
@@ -135,15 +136,15 @@ elseif(dclass == H5T_STRING_F) then
     endif
 
     call h5tclose_f(type_id, ier)
-    if(ier/=0) error stop "h5fortran:read:h5tclose " // dname // " in " // self%filename
+    if(ier/=0) error stop "ERROR:h5fortran:read:h5tclose " // dname // " in " // self%filename
 
   class default
-    error stop "h5fortran:read: character disk dataset " // dname // " needs character memory variable"
+    error stop "ERROR:h5fortran:read: character disk dataset " // dname // " needs character memory variable"
   end select
 else
-  error stop 'h5fortran:reader: non-handled datatype--please reach out to developers.'
+  error stop 'ERROR:h5fortran:reader: non-handled datatype--please reach out to developers.'
 end if
-if(ier/=0) error stop 'h5fortran:reader: reading ' // dname // ' from ' // self%filename
+if(ier/=0) error stop 'ERROR:h5fortran:reader: reading ' // dname // ' from ' // self%filename
 
 call h5dclose_f(dset_id, ier)
 if(ier /= 0) error stop "ERROR:h5fortran:reader: closing dataset: " // dname // " in " // self%filename
