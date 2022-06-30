404cb694eb55312adc486a7eb93505f3bff0ef57
Michael Hirsch <scivision@users.noreply.github.com>
2022-06-29 22:02:16 -0400

    test_attributes.py: write test data

---
+++ test/test_attributes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+write attributes to ensure h5fortran can read
+"""
+
+import argparse
+from pathlib import Path
+
+import numpy as np
+import h5py
+
+p = argparse.ArgumentParser(description="write test attributes in HDF5")
+p.add_argument("file", help="HDF5 file to write")
+P = p.parse_args()
+
+fn = Path(P.file).expanduser()
+
+with h5py.File(fn, "w") as f:
+    v = f.create_dataset("/empty", dtype=float)
+    v.attrs["real32-scalar"] = np.float32(3.)
+    v.attrs["real64-scalar"] = np.float64(3.)
+    v.attrs["real16-scalar"] = np.float16(3.)
