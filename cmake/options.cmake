d1fe450aa7bf5f070b21b20757b106532e0782d3
Michael Hirsch <scivision@users.noreply.github.com>
2022-07-08 10:11:42 -0400

    cmake: config proper install filename

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -20,12 +20,6 @@ endif()
 
 cmake_path(SET CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/Modules)
 
-if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-  # will not take effect without FORCE
-  # CMAKE_BINARY_DIR in case it's used from FetchContent
-  set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR} CACHE PATH "Install top-level directory" FORCE)
-endif()
-
 # Rpath options necessary for shared library install to work correctly in user projects
 set(CMAKE_INSTALL_NAME_DIR ${CMAKE_INSTALL_PREFIX}/lib)
 set(CMAKE_INSTALL_RPATH ${CMAKE_INSTALL_PREFIX}/lib)
@@ -33,6 +27,17 @@ set(CMAKE_INSTALL_RPATH_USE_LINK_PATH true)
 
 # Necessary for shared library with Visual Studio / Windows oneAPI
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
+
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+  # will not take effect without FORCE
+  # CMAKE_BINARY_DIR for use from FetchContent
+  set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR} CACHE PATH "Install top-level directory" FORCE)
+endif()
+
+# allow CMAKE_PREFIX_PATH with ~ expand
+if(CMAKE_PREFIX_PATH)
+  get_filename_component(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ABSOLUTE)
+endif()
 
 # --- auto-ignore build directory
 if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
