2143db371cccf4b2ec77a0a02abd64c0b7ee1ae5
Michael Hirsch <scivision@users.noreply.github.com>
2022-08-17 15:12:06 -0400

    read_fixed.inc dedupe

--- src/read_ascii.f90
+++ src/read_ascii.f90
@@ -30,9 +30,9 @@ charlen = len(A)
 call open_char(self, obj_name, obj_id, file_space_id, charlen, obj_type, type_id, dims, Npts, dsize, is_vlen)
 
 if(is_vlen) then
-  call read_vlen(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
+  call read_vlen(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 else
-  call read_fixed(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
+  call read_fixed(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 endif
 
 call H5Tclose_f(type_id, ier)
@@ -69,21 +69,21 @@ include 'read_char.inc'
 end procedure
 
 
-subroutine read_fixed0(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
+subroutine read_fixed0(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 
 class(hdf5_file), intent(in) :: self
 character(*), intent(in) :: obj_name
 integer, intent(in) :: obj_type
 integer(HID_T), intent(in) :: obj_id, type_id, mem_space_id, file_space_id
-integer(HSIZE_T), intent(in) :: dims(1), dsize
+integer(HSIZE_T), intent(in) :: dsize
 character(*), intent(inout) :: A
 
 TYPE(C_PTR) :: f_ptr
-CHARACTER(:), DIMENSION(:), ALLOCATABLE, TARGET :: buf
+CHARACTER(len=dsize), DIMENSION(:), ALLOCATABLE, TARGET :: buf
 
 integer :: ier
 
-allocate(character(dsize) :: buf(dims(1)))
+allocate(buf(1))
 
 f_ptr = C_LOC(buf)
 
@@ -98,211 +98,64 @@ A = pad_trim(buf(1))
 
 end subroutine read_fixed0
 
-subroutine read_fixed1(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
-
-class(hdf5_file), intent(in) :: self
-character(*), intent(in) :: obj_name
-integer, intent(in) :: obj_type
-integer(HID_T), intent(in) :: obj_id, type_id, mem_space_id, file_space_id
+subroutine read_fixed1(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 character(*), intent(inout), dimension(:) :: A
-integer(HSIZE_T), intent(in) :: dims(rank(A)), dsize
+integer(HSIZE_T), intent(in) :: dsize
+CHARACTER(len=dsize), DIMENSION(:), ALLOCATABLE, TARGET :: buf
+include 'read_fixed.inc'
+end subroutine
 
-TYPE(C_PTR) :: f_ptr
-CHARACTER(:), DIMENSION(:), ALLOCATABLE, TARGET :: buf
-
-integer :: ier
-
-allocate(character(dsize) :: buf(dims(1)))
-f_ptr = C_LOC(buf)
-
-if(obj_type == H5I_DATASET_F) then
-  call H5Dread_f(obj_id, type_id, f_ptr, ier, mem_space_id, file_space_id)
-elseif(obj_type == H5I_ATTR_F) then
-  call H5Aread_f(obj_id, type_id, f_ptr, ier)
-endif
-if(ier/=0) error stop "ERROR:h5fortran:read_ascii: read " // obj_name // " " // self%filename
-
-A = pad_trim(buf)
-
-end subroutine read_fixed1
-
-subroutine read_fixed2(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
-
-class(hdf5_file), intent(in) :: self
-character(*), intent(in) :: obj_name
-integer, intent(in) :: obj_type
-integer(HID_T), intent(in) :: obj_id, type_id, mem_space_id, file_space_id
+subroutine read_fixed2(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 character(*), intent(inout), dimension(:,:) :: A
-integer(HSIZE_T), intent(in) :: dims(rank(A)), dsize
-
-TYPE(C_PTR) :: f_ptr
-CHARACTER(:), DIMENSION(:,:), ALLOCATABLE, TARGET :: buf
-
-integer :: ier
-
-allocate(character(dsize) :: buf(dims(1), dims(2)))
-f_ptr = C_LOC(buf)
-
-if(obj_type == H5I_DATASET_F) then
-  call H5Dread_f(obj_id, type_id, f_ptr, ier, mem_space_id, file_space_id)
-elseif(obj_type == H5I_ATTR_F) then
-  call H5Aread_f(obj_id, type_id, f_ptr, ier)
-endif
-if(ier/=0) error stop "ERROR:h5fortran:read_ascii: read " // obj_name // " " // self%filename
-
-A = pad_trim(buf)
-
-end subroutine read_fixed2
+integer(HSIZE_T), intent(in) :: dsize
+CHARACTER(len=dsize), DIMENSION(:,:), ALLOCATABLE, TARGET :: buf
+include 'read_fixed.inc'
+end subroutine
 
 
-subroutine read_fixed3(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
-
-class(hdf5_file), intent(in) :: self
-character(*), intent(in) :: obj_name
-integer, intent(in) :: obj_type
-integer(HID_T), intent(in) :: obj_id, type_id, mem_space_id, file_space_id
+subroutine read_fixed3(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 character(*), intent(inout), dimension(:,:,:) :: A
-integer(HSIZE_T), intent(in) :: dims(rank(A)), dsize
+integer(HSIZE_T), intent(in) :: dsize
+CHARACTER(len=dsize), DIMENSION(:,:,:), ALLOCATABLE, TARGET :: buf
+include 'read_fixed.inc'
+end subroutine
 
-TYPE(C_PTR) :: f_ptr
-CHARACTER(:), DIMENSION(:,:,:), ALLOCATABLE, TARGET :: buf
-
-integer :: ier
-
-allocate(character(dsize) :: buf(dims(1), dims(2), dims(3)))
-f_ptr = C_LOC(buf)
-
-if(obj_type == H5I_DATASET_F) then
-  call H5Dread_f(obj_id, type_id, f_ptr, ier, mem_space_id, file_space_id)
-elseif(obj_type == H5I_ATTR_F) then
-  call H5Aread_f(obj_id, type_id, f_ptr, ier)
-endif
-if(ier/=0) error stop "ERROR:h5fortran:read_ascii: read " // obj_name // " " // self%filename
-
-A = pad_trim(buf)
-
-end subroutine read_fixed3
-
-subroutine read_fixed4(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
-
-class(hdf5_file), intent(in) :: self
-character(*), intent(in) :: obj_name
-integer, intent(in) :: obj_type
-integer(HID_T), intent(in) :: obj_id, type_id, mem_space_id, file_space_id
+subroutine read_fixed4(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 character(*), intent(inout), dimension(:,:,:,:) :: A
-integer(HSIZE_T), intent(in) :: dims(rank(A)), dsize
+integer(HSIZE_T), intent(in) :: dsize
+CHARACTER(len=dsize), DIMENSION(:,:,:,:), ALLOCATABLE, TARGET :: buf
+include 'read_fixed.inc'
+end subroutine
 
-TYPE(C_PTR) :: f_ptr
-CHARACTER(:), DIMENSION(:,:,:,:), ALLOCATABLE, TARGET :: buf
-
-integer :: ier
-
-allocate(character(dsize) :: buf(dims(1), dims(2), dims(3), dims(4)))
-f_ptr = C_LOC(buf)
-
-if(obj_type == H5I_DATASET_F) then
-  call H5Dread_f(obj_id, type_id, f_ptr, ier, mem_space_id, file_space_id)
-elseif(obj_type == H5I_ATTR_F) then
-  call H5Aread_f(obj_id, type_id, f_ptr, ier)
-endif
-if(ier/=0) error stop "ERROR:h5fortran:read_ascii: read " // obj_name // " " // self%filename
-
-A = pad_trim(buf)
-
-end subroutine read_fixed4
-
-subroutine read_fixed5(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
-
-class(hdf5_file), intent(in) :: self
-character(*), intent(in) :: obj_name
-integer, intent(in) :: obj_type
-integer(HID_T), intent(in) :: obj_id, type_id, mem_space_id, file_space_id
+subroutine read_fixed5(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 character(*), intent(inout), dimension(:,:,:,:,:) :: A
-integer(HSIZE_T), intent(in) :: dims(rank(A)), dsize
+integer(HSIZE_T), intent(in) :: dsize
+CHARACTER(len=dsize), DIMENSION(:,:,:,:,:), ALLOCATABLE, TARGET :: buf
+include 'read_fixed.inc'
+end subroutine
 
-TYPE(C_PTR) :: f_ptr
-CHARACTER(:), DIMENSION(:,:,:,:,:), ALLOCATABLE, TARGET :: buf
-
-integer :: ier
-
-allocate(character(dsize) :: buf(dims(1), dims(2), dims(3), dims(4), dims(5)))
-f_ptr = C_LOC(buf)
-
-if(obj_type == H5I_DATASET_F) then
-  call H5Dread_f(obj_id, type_id, f_ptr, ier, mem_space_id, file_space_id)
-elseif(obj_type == H5I_ATTR_F) then
-  call H5Aread_f(obj_id, type_id, f_ptr, ier)
-endif
-if(ier/=0) error stop "ERROR:h5fortran:read_ascii: read " // obj_name // " " // self%filename
-
-A = pad_trim(buf)
-
-end subroutine read_fixed5
-
-subroutine read_fixed6(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
-
-class(hdf5_file), intent(in) :: self
-character(*), intent(in) :: obj_name
-integer, intent(in) :: obj_type
-integer(HID_T), intent(in) :: obj_id, type_id, mem_space_id, file_space_id
+subroutine read_fixed6(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 character(*), intent(inout), dimension(:,:,:,:,:,:) :: A
-integer(HSIZE_T), intent(in) :: dims(rank(A)), dsize
+integer(HSIZE_T), intent(in) :: dsize
+CHARACTER(len=dsize), DIMENSION(:,:,:,:,:,:), ALLOCATABLE, TARGET :: buf
+include 'read_fixed.inc'
+end subroutine
 
-TYPE(C_PTR) :: f_ptr
-CHARACTER(:), DIMENSION(:,:,:,:,:,:), ALLOCATABLE, TARGET :: buf
-
-integer :: ier
-
-allocate(character(dsize) :: buf(dims(1), dims(2), dims(3), dims(4), dims(5), dims(6)))
-f_ptr = C_LOC(buf)
-
-if(obj_type == H5I_DATASET_F) then
-  call H5Dread_f(obj_id, type_id, f_ptr, ier, mem_space_id, file_space_id)
-elseif(obj_type == H5I_ATTR_F) then
-  call H5Aread_f(obj_id, type_id, f_ptr, ier)
-endif
-if(ier/=0) error stop "ERROR:h5fortran:read_ascii: read " // obj_name // " " // self%filename
-
-A = pad_trim(buf)
-
-end subroutine read_fixed6
-
-subroutine read_fixed7(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
-
-class(hdf5_file), intent(in) :: self
-character(*), intent(in) :: obj_name
-integer, intent(in) :: obj_type
-integer(HID_T), intent(in) :: obj_id, type_id, mem_space_id, file_space_id
+subroutine read_fixed7(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 character(*), intent(inout), dimension(:,:,:,:,:,:,:) :: A
-integer(HSIZE_T), intent(in) :: dims(rank(A)), dsize
-
-TYPE(C_PTR) :: f_ptr
-CHARACTER(:), DIMENSION(:,:,:,:,:,:,:), ALLOCATABLE, TARGET :: buf
-
-integer :: ier
-
-allocate(character(dsize) :: buf(dims(1), dims(2), dims(3), dims(4), dims(5), dims(6), dims(7)))
-f_ptr = C_LOC(buf)
-
-if(obj_type == H5I_DATASET_F) then
-  call H5Dread_f(obj_id, type_id, f_ptr, ier, mem_space_id, file_space_id)
-elseif(obj_type == H5I_ATTR_F) then
-  call H5Aread_f(obj_id, type_id, f_ptr, ier)
-endif
-if(ier/=0) error stop "ERROR:h5fortran:read_ascii: read " // obj_name // " " // self%filename
-
-A = pad_trim(buf)
-
-end subroutine read_fixed7
+integer(HSIZE_T), intent(in) :: dsize
+CHARACTER(len=dsize), DIMENSION(:,:,:,:,:,:,:), ALLOCATABLE, TARGET :: buf
+include 'read_fixed.inc'
+end subroutine
 
 
-subroutine read_vlen0(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
+subroutine read_vlen0(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 
 class(hdf5_file), intent(in) :: self
 character(*), intent(in) :: obj_name
 integer, intent(in) :: obj_type
 integer(HID_T), intent(in) :: obj_id, type_id, mem_space_id, file_space_id
-integer(HSIZE_T), intent(in) :: dims(1), dsize
+integer(HSIZE_T), intent(in) :: dsize
 character(*), intent(inout) :: A
 
 TYPE(C_PTR), DIMENSION(:), ALLOCATABLE, TARGET :: cbuf
@@ -311,7 +164,7 @@ TYPE(C_PTR) :: f_ptr
 
 integer :: ier
 
-allocate(cbuf(dims(1)))
+allocate(cbuf(1))
 f_ptr = C_LOC(cbuf)
 
 if(obj_type == H5I_DATASET_F) then
@@ -327,37 +180,37 @@ A = pad_trim(cstr)
 
 end subroutine read_vlen0
 
-subroutine read_vlen1(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
+subroutine read_vlen1(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 character(*), intent(inout), dimension(:) :: A
 include 'read_vlen.inc'
 end subroutine
 
-subroutine read_vlen2(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
+subroutine read_vlen2(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 character(*), intent(inout), dimension(:,:) :: A
 include 'read_vlen.inc'
 end subroutine
 
-subroutine read_vlen3(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
+subroutine read_vlen3(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 character(*), intent(inout), dimension(:,:,:) :: A
 include 'read_vlen.inc'
 end subroutine
 
-subroutine read_vlen4(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
+subroutine read_vlen4(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 character(*), intent(inout), dimension(:,:,:,:) :: A
 include 'read_vlen.inc'
 end subroutine
 
-subroutine read_vlen5(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
+subroutine read_vlen5(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 character(*), intent(inout), dimension(:,:,:,:,:) :: A
 include 'read_vlen.inc'
 end subroutine
 
-subroutine read_vlen6(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
+subroutine read_vlen6(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 character(*), intent(inout), dimension(:,:,:,:,:,:) :: A
 include 'read_vlen.inc'
 end subroutine
 
-subroutine read_vlen7(self, obj_name, obj_type, obj_id, type_id, dims, dsize, A, mem_space_id, file_space_id)
+subroutine read_vlen7(self, obj_name, obj_type, obj_id, type_id, dsize, A, mem_space_id, file_space_id)
 character(*), intent(inout), dimension(:,:,:,:,:,:,:) :: A
 include 'read_vlen.inc'
 end subroutine
