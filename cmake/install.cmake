7e2748a2443f2cb18ea5646d5f81cf624500b116
Michael Hirsch <scivision@users.noreply.github.com>
2022-08-31 00:29:04 -0400

    cpack tbz2

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -25,14 +25,13 @@ DESTINATION cmake
 
 # --- CPack
 
-set(CPACK_GENERATOR "TZST")
-set(CPACK_SOURCE_GENERATOR "TZST")
+set(CPACK_GENERATOR "TBZ2")
+set(CPACK_SOURCE_GENERATOR "TBZ2")
 set(CPACK_PACKAGE_VENDOR "Michael Hirsch")
 set(CPACK_PACKAGE_CONTACT "Michael Hirsch")
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "libhdf5-dev (>=1.10)")
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
 set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
-set(CPACK_PACKAGE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/package)
 
 # not .gitignore as its regex syntax is more advanced than CMake
 set(CPACK_SOURCE_IGNORE_FILES .git/ .github/ .vscode/ .mypy_cache/ _CPack_Packages/
