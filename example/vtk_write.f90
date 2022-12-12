c4960f810cc315fcc1e6cc1c90f6ff4000c266cb
Michael Hirsch <scivision@users.noreply.github.com>
2022-12-12 01:05:20 -0500

    example:vtk: version header
    vtk: imageData example"

--- example/vtk_write.f90
+++ example/vtk_write.f90
@@ -17,14 +17,39 @@ implicit none
 character(1024) :: filename
 integer :: ierr
 
+!> these are just to avoid typos, you can type out full name each time
+character(7), parameter :: vtkhdf = "/VTKHDF"
+character(17), parameter :: pointdata = vtkhdf // "/PointData"
+
 type(hdf5_file) :: h
+
+real, allocatable :: Iterations(:,:,:), IterationsGradient(:,:,:,:)
 
 call get_command_argument(1, filename, status=ierr)
 if(ierr /= 0) error stop "please give filename to write"
 
 call h % open(filename, 'w')
 
-call h % write("/VTKHDF/Version", [1, 0])
+call h % create_group(vtkhdf)
+call h % writeattr(vtkhdf ,"Version", [1, 0])
+
+!> ImageData example
+call h % writeattr(vtkhdf, "Direction", [1, 0, 0, 0, 1, 0, 0, 0, 1])
+call h % writeattr(vtkhdf, "Origin", [-1.75, -1.25, 0.])
+call h % writeattr(vtkhdf, "Spacing", [0.131579, 0.125, 0.0952381])
+call h % writeattr(vtkhdf, "WholeExtent", [0, 19, 0, 20, 0, 21])
+call h % writeattr(vtkhdf, "Type", "ImageData")
+call h % create_group(pointdata)
+call h % writeattr(pointdata, "Scalars", "IterationsGradient")
+
+allocate(Iterations(20, 21, 22))
+call random_number(Iterations)
+call h % write(pointdata // "/Iterations", Iterations)
+deallocate(Iterations)
+
+! allocate(IterationsGradient(3, 20, 21, 22))
+! call h % write(pointdata // "/IterationsGradient", IterationsGradient)
+! deallocate(IterationsGradient)
 
 call h % close()
 
