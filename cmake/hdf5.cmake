geospace-code/h5fortran

commit b86486716c1f768b94e15498dfe1d10aa0676af6

json: simplify url specification

cmake/hdf5.cmake
@@ -4,6 +4,10 @@
 include(GNUInstallDirs)
 include(ExternalProject)
 
+if(NOT DEFINED hdf5_key)
+  set(hdf5_key hdf5-1.10)
+endif()
+
 if(hdf5_parallel)
   find_package(MPI REQUIRED COMPONENTS C)
 endif()
@@ -74,7 +78,7 @@ if(MPI_ROOT)
 endif()
 
 if(NOT hdf5_url)
-  string(JSON hdf5_url GET ${json} hdf5 url)
+  string(JSON hdf5_url GET ${json} ${hdf5_key})
 endif()
 
 # Get HDF5 version from underscore-separated version in URL

cmake/zlib.cmake
@@ -4,11 +4,7 @@ include(GNUInstallDirs)
 include(ExternalProject)
 
 if(NOT zlib_url)
-if(zlib_legacy)
-  string(JSON zlib_url GET ${json} zlib1 url)
-else()
-  string(JSON zlib_url GET ${json} zlib2 url)
-endif()
+string(JSON zlib_url GET ${json} ${zlib_key})
 endif()
 
 set(ZLIB_INCLUDE_DIRS ${CMAKE_INSTALL_FULL_INCLUDEDIR})
@@ -20,7 +16,7 @@ if(BUILD_SHARED_LIBS)
     set(ZLIB_LIBRARIES ${CMAKE_INSTALL_FULL_LIBDIR}/${CMAKE_SHARED_LIBRARY_PREFIX}z${CMAKE_SHARED_LIBRARY_SUFFIX})
   endif()
 else()
-  if(MSVC OR (WIN32 AND zlib_legacy))
+  if(MSVC OR (WIN32 AND zlib_key STREQUAL "zlib1"))
     set(ZLIB_LIBRARIES ${CMAKE_INSTALL_FULL_LIBDIR}/${CMAKE_STATIC_LIBRARY_PREFIX}zlibstatic${CMAKE_STATIC_LIBRARY_SUFFIX})
   else()
     set(ZLIB_LIBRARIES ${CMAKE_INSTALL_FULL_LIBDIR}/${CMAKE_STATIC_LIBRARY_PREFIX}z${CMAKE_STATIC_LIBRARY_SUFFIX})

scripts/CMakeLists.txt
@@ -6,7 +6,6 @@ LANGUAGES C Fortran
 )
 
 option(hdf5_parallel "build HDF5 parallel MPI")
-option(zlib_legacy "use legacy zlib 1.x")
 option(build_zlib "build zlib")
 
 # --- system checks

commit 3d19bac7af5fd6a4964f115ba669c0eb47dea1b0

fix Zlib build bug introduced in b864867

cmake/hdf5.cmake
@@ -38,13 +38,16 @@ file(READ ${CMAKE_CURRENT_LIST_DIR}/libraries.json json)
 # --- Zlib
 if(build_zlib)
   set(zlib_dep DEPENDS ZLIB)
-  if(TARGET ZLIB::ZLIB)
-    add_custom_target(ZLIB)
-  else()
-    include(${CMAKE_CURRENT_LIST_DIR}/zlib.cmake)
-  endif()
 else()
   set(zlib_dep)
+endif()
+
+if(TARGET ZLIB::ZLIB)
+  add_custom_target(ZLIB)
+elseif(h5fortran_find)
+  find_package(ZLIB)
+else()
+  include(${CMAKE_CURRENT_LIST_DIR}/zlib.cmake)
 endif()
 
 # --- HDF5

cmake/zlib.cmake
@@ -3,6 +3,10 @@
 include(GNUInstallDirs)
 include(ExternalProject)
 
+if(NOT DEFINED zlib_key)
+  set(zlib_key zlib2)
+endif()
+
 if(NOT zlib_url)
 string(JSON zlib_url GET ${json} ${zlib_key})
 endif()