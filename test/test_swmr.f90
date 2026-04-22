39d7847230de220559730b6d42b4a6c798d0cfda
Ian Giestas Pauli <iangiestas@usp.br>
2026-04-22 12:05:29 -0300

    test: SWMR write/update/read test

--- test/test_swmr.f90
+++ test/test_swmr.f90
@@ -3,7 +3,7 @@ program test_swmr
   implicit none
 
   character(*), parameter :: fn = 'test_swmr.h5'
-  type(hdf5_file) :: h
+  type(hdf5_file) :: h_write, h_read
   integer, parameter :: n = 10
   real :: dat(n), dat_out(n)
   integer :: i
@@ -12,22 +12,30 @@ program test_swmr
     dat(i) = real(i)
   end do
 
-  ! Write with swmr mode
-  call h%open(fn, action='w', swmr=.true.)
-  call h%write('/data', dat)
-  call h%flush('/data')
-  call h%close()
+  ! Writer: create with swmr mode
+  call h_write%open(fn, action='w', swmr=.true.)
+  call h_write%write('/data', dat)
+  call h_write%flush('/data')
 
-  ! Read (without swmr flag)
-  call h%open(fn, action='r')
-  call h%read('/data', dat_out)
-  call h%close()
+  ! Writer: update data while still open
+  dat = dat * 2.0
+  call h_write%write('/data', dat)
+  call h_write%flush('/data')
+  call h_write%close()
+
+  ! Reader: open file created with swmr mode
+  call h_read%open(fn, action='r')
+  call h_read%refresh('/data')
+  call h_read%read('/data', dat_out)
+  call h_read%close()
 
   if (any(dat_out /= dat)) then
     print '(A)', 'ERROR: data mismatch'
+    print '(A,10F5.2)', 'Expected:', dat
+    print '(A,10F5.2)', 'Got:     ', dat_out
     call exit(1)
   end if
 
-  print '(A)', 'PASSED'
+  print '(A)', 'PASSED: SWMR write/update/read'
 
 end program test_swmr
