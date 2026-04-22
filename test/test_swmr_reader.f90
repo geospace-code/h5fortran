bf2f435ad7d8a49422cddde9bfac3a1da9793871
Ian Giestas Pauli <iangiestas@usp.br>
2026-04-22 10:47:15 -0300

    feat: concurrent SWMR writer/reader with timestamps

--- test/test_swmr_reader.f90
+++ test/test_swmr_reader.f90
@@ -6,15 +6,25 @@ program test_swmr_reader
   type(hdf5_file) :: h
   integer, parameter :: n = 10
   real :: dat(n)
-  integer :: k
+  integer :: k, c1, cr
+  logical :: ok
 
-  call h%open(fn, action='r', swmr=.true.)
+  call system_clock(c1, cr)
+
+  ! Try to open, exit if file not ready
+  call h%open(fn, action='r', swmr=.true., ok=ok)
+  if (.not. ok) then
+    print *, 'READER: file not ready'
+    call exit(1)
+  end if
+
   k = 0
   do
     k = k + 1
     call h%refresh('/data')
     call h%read('/data', dat)
-    print *, 'READER:', k, 'got', dat(1), '...', dat(n)
+    call system_clock(cr)
+    print '(A,I0,A,F6.3,A,F5.2)', 'READER:', k, ' t=', real(cr-c1)/1000, ' v=', dat(1)
     if (k >= 5) exit
   end do
   call h%close()
--- test/test_swmr_writer.f90
+++ test/test_swmr_writer.f90
@@ -4,21 +4,28 @@ program test_swmr_writer
 
   character(*), parameter :: fn = 'swmr.h5'
   type(hdf5_file) :: h
-  integer, parameter :: n = 10, niter = 3
+  integer, parameter :: n = 10, niter = 5
   real :: dat(n)
   integer :: i, k
+  integer :: c1, cr
 
-  ! Create file first
+  call system_clock(c1, cr)
+
+  ! Create file first (so reader can open)
   call h%open(fn, action='w', swmr=.true.)
   do i = 1, n
     dat(i) = real(i)
   end do
   call h%write('/data', dat)
   call h%flush('/data')
-  print *, 'WRITER: created'
   call h%close()
+  call system_clock(cr)
+  print '(A,F6.3)', 'WRITER: created t=', real(cr-c1)/1000
 
-  ! Then write in loop (simulating continuous data)
+  ! Wait for reader to start
+  call execute_command_line('sleep 1')
+
+  ! Now write updates while reader reads
   call h%open(fn, action='r+', swmr=.true.)
   do k = 1, niter
     do i = 1, n
@@ -26,9 +33,10 @@ program test_swmr_writer
     end do
     call h%write('/data', dat)
     call h%flush('/data')
-    print *, 'WRITER:', k, 'wrote', dat(1)
+    call system_clock(cr)
+    print '(A,I0,A,F6.3)', 'WRITER:', k, ' t=', real(cr-c1)/1000
   end do
   call h%close()
-  print *, 'WRITER: done'
+  print '(A,F6.3)', 'WRITER: done'
 
 end program test_swmr_writer
